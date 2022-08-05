fa7f45ec2a0c1ab665b0eca5e39d4f87a1787559
Jouda Hidri <joudahidri@Joudas-MacBook-Pro.local>
2022-08-05 17:36:41 +0200

    Put merge as seperate method

--- lsm.py
+++ lsm.py
@@ -34,39 +34,48 @@ def write():
   global segments
   path = str(datetime.timestamp(datetime.now())) # todo current segment also should be global
   segments.append(path) # todo create new segment only when current is too big
+  print("> segments size="+str(len(segments)))
   with open(path, 'w') as f:
     writeTree(root, f)
     root = None
 
-for line in sys.stdin:
-  if line == "write\n": # todo auto merge when file size reaches some threshold
-    write()
-  elif line == "merge\n": # todo auto merge after 4 segments, to have a fixed number of segments
-      with open(segments[0], 'r') as segment1, open(segments[1], 'r') as segment2, open("sstable", 'w') as sstable:
-        lines1 = segment1.readlines()
-        lines2 = segment2.readlines()
-        i = 0
-        j = 0
-        while ( i < len(lines1) and j < len(lines2)):
-          line1IsDuplicate = i+1 < len(lines1) and lines1[i] == lines1 [i+1]
-          line2IsDuplicate = j+1 < len(lines2) and lines2[j] == lines2 [j+1]
-          if(line1IsDuplicate or line2IsDuplicate):
-            if(line1IsDuplicate):
-              i+=1
-            if(line2IsDuplicate):
-              j+=1
-          elif (lines1[i] <= lines2[j]):
-            sstable.write(lines1[i])
-            i+=1
-          else:
-            sstable.write(lines2[j])
-            j+=1
-        while ( i < len(lines1) ):
-          sstable.write(lines1[i])
+def merge():
+  global segments
+  with open(segments[0], 'r') as segment1, open(segments[1], 'r') as segment2, open('sstable', 'w') as sstable:
+    lines1 = segment1.readlines()
+    lines2 = segment2.readlines()
+    i = 0
+    j = 0
+    while ( i < len(lines1) and j < len(lines2)):
+      line1IsDuplicate = i+1 < len(lines1) and lines1[i] == lines1 [i+1]
+      line2IsDuplicate = j+1 < len(lines2) and lines2[j] == lines2 [j+1]
+      if(line1IsDuplicate or line2IsDuplicate):
+        if(line1IsDuplicate):
           i+=1
-        while ( j < len(lines2) ):
-          sstable.write(lines2[j])
+        if(line2IsDuplicate):
           j+=1
+      elif (lines1[i] <= lines2[j]):
+        sstable.write(lines1[i])
+        i+=1
+      else:
+        sstable.write(lines2[j])
+        j+=1
+    while ( i < len(lines1) ):
+      sstable.write(lines1[i])
+      i+=1
+    while ( j < len(lines2) ):
+      sstable.write(lines2[j])
+      j+=1
+  print("----------")
+  with open('sstable', 'r') as f:
+      print(f.read())
+  print("----------")
+
+for line in sys.stdin:
+  if line == "write\n":
+    write()
+  elif line == "merge\n":
+    merge()
   elif line == "exit\n":
     break
   else:
