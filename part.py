bc8336713e2be61ef86845835a236c469c074c0a
Jouda Hidri <joudahidri@Joudas-MacBook-Pro.local>
2022-08-18 13:01:37 +0100

    Implement partitions

--- part.py
+++ part.py
@@ -1,14 +1,22 @@
 import sys
 
+partitions = 3
+
 def getKey(line):
   return line.split(":")[0]
 
+def getPartition(key):
+  partition = int(key) % partitions
+  print(str(partitions)+" partitions: key="+key+" partition="+str(partition))
+  return partition
+
+
 def write(line):
-  with open("partitions/1", 'a') as f:
+  with open("partitions/"+str(getPartition(getKey(line))), 'a') as f: #todo select partition based on key
     f.write(line)
 
 def read(key):
-  with open('partitions/1', 'r') as f:
+  with open('partitions/'+str(getPartition(key)), 'r') as f:
     lines = f.readlines()
     i = 0
     result = None
@@ -21,6 +29,10 @@ def read(key):
 for line in sys.stdin:
   if line == "exit\n":
     break
+  elif line.startswith("partitions "):
+    partitions = int(line.replace("partitions ", "").replace("\n", ""))
+    if partitions < 1:
+      partitions = 1
   elif line.startswith("read "):
     key = line.replace('read ', '').replace("\n", "")
     result = read(key)
